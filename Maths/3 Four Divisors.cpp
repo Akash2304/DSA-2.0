3b833e6784152dd0d5bbbe8aef00465c61b16ec8
Akash Rajpoot <54078472+Akash2304@users.noreply.github.com>
2023-07-31 23:46:27 +0530

    Create 3 Four Divisors.cpp

---
+++ Maths/3 Four Divisors.cpp
@@ -0,0 +1,62 @@
+/************************************************************************************************************************************************************************
+Given an integer array nums, return the sum of divisors of the integers in that array that have exactly four divisors. If there is no such integer in the array, return 0.
+
+ 
+
+Example 1:
+
+Input: nums = [21,4,7]
+Output: 32
+Explanation: 
+21 has 4 divisors: 1, 3, 7, 21
+4 has 3 divisors: 1, 2, 4
+7 has 2 divisors: 1, 7
+The answer is the sum of divisors of 21 only.
+Example 2:
+
+Input: nums = [21,21]
+Output: 64
+Example 3:
+
+Input: nums = [1,2,3,4,5]
+Output: 0
+ 
+
+Constraints:
+
+1 <= nums.length <= 104
+1 <= nums[i] <= 105
+***********************************************************************************************************************************************************************/
+
+class Solution {
+public:
+    int sumFourDivisors(vector<int>& nums) {
+         int i,j,k,sum=0;
+        for(i=0;i<nums.size();i++)
+        {   
+            // bsum: bsum stores the sum of default divisors i.e. 1 and the number itself
+            // t: used to count the number of divisors present 
+            // n: used to store half of the number (no divisors will be present if the control exceeds n)
+            // sn: used to store 1/4th of the number (or half of n)
+            int bsum=1+nums[i], t=0, n=nums[i]/2, sn=n/2;
+
+            //Calculating remaining divisors
+            for(j=2;j<=n;j++)
+            {  
+                if(nums[i]%j==0){
+                t++;
+                bsum+=j;
+                }
+                //Condition t>2: If the total number of diviors exceeds 2 then number will be discarded, values of t and bsum will be initialized again
+                //Condition t==0&&j>=sn: If 2 diviors are not found within the 1st half of the range, then 2 diviors wont be present in the 2nd half  
+                if(t>2||(t==0&&j>=sn))
+                break;
+            }
+            //Checking if the number of divisors obtained are exactly 2
+            if(t==2)
+                //Adding bsum into final sum, as bsum contains the soultion for 1 number but we are presented with an array of numbers
+                    sum+=bsum;
+        }
+        return sum;
+    }
+};
