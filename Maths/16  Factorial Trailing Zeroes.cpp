3d3b4068c48157fd4d9d189df0e5bcd94ef372b4
Akash Rajpoot <54078472+Akash2304@users.noreply.github.com>
2023-08-16 13:16:58 +0530

    Update 16  Factorial Trailing Zeroes.cpp

--- Maths/16  Factorial Trailing Zeroes.cpp
+++ Maths/16  Factorial Trailing Zeroes.cpp
@@ -92,3 +92,14 @@ In summary, i *= 5 is a mechanism to ensure that the loop considers all the nece
 in the factorial.
 
 /***********************************************************************************************************************************************************************/
+class Solution {
+public:
+    int trailingZeroes(int n) {
+        int ans = 0;
+        while (n>0){
+            ans += n/5;
+            n = n/5;
+        }
+        return ans;
+    }
+};
