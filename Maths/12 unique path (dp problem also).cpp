5547f96b467e3a021e5a4acd1ee0aa31afdc7cb5
Akash Rajpoot <54078472+Akash2304@users.noreply.github.com>
2023-08-18 11:53:12 +0530

    Update 12 unique path (dp problem also).cpp

--- Maths/12 unique path (dp problem also).cpp
+++ Maths/12 unique path (dp problem also).cpp
@@ -90,7 +90,7 @@ Code
 class Solution {
     public:
     int uniquePaths(int m, int n) {
-        int N = m + n - 2;
+        int N = m + n - 2; // m-1 + n-1 =m+n-2
         int r = (m-1 < n-1) ? m-1 : n-1;
         double res = 1;
         for(int i = 1; i <= r; i++) {
